4cfa2b26009badae4165b991df37218ee91bf656
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-02-19 09:35:32 -0400

    feat: add db shedcn initial setup and components

---
+++ src/app/product/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPace = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>tetse</Button>
+      <Input />
+    </div>
+  );
+};
+
+export default ProductPace;
