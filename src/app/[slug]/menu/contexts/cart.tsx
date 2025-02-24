d8113263bf7a0ded03c1774b590b789be570bc9f
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-02-24 12:18:18 -0400

    feat: ensure cart summary is shown on restaurat page

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
@@ -6,21 +7,24 @@ export interface CartProduct
   extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
+
 export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   total: number;
+  totalQuantity: number;
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
-  increaseProductQuantity: (pruductId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
   removeProduct: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
-  products: [],
   total: 0,
+  totalQuantity: 0,
+  products: [],
   toggleCart: () => {},
   addProduct: () => {},
   decreaseProductQuantity: () => {},
@@ -35,16 +39,17 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const total = products.reduce((acc, product) => {
     return acc + product.price * product.quantity;
   }, 0);
-
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
   const toggleCart = () => {
     setIsOpen((prev) => !prev);
   };
-
   const addProduct = (product: CartProduct) => {
-    const productsIsAlreadyOnTheCart = products.some(
+    const productIsAlreadyOnTheCart = products.some(
       (prevProduct) => prevProduct.id === product.id,
     );
-    if (!productsIsAlreadyOnTheCart) {
+    if (!productIsAlreadyOnTheCart) {
       return setProducts((prev) => [...prev, product]);
     }
     setProducts((prevProducts) => {
@@ -62,7 +67,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const decreaseProductQuantity = (productId: string) => {
     setProducts((prevProducts) => {
       return prevProducts.map((prevProduct) => {
-        if (prevProduct.id != productId) {
+        if (prevProduct.id !== productId) {
           return prevProduct;
         }
         if (prevProduct.quantity === 1) {
@@ -72,20 +77,19 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       });
     });
   };
-
   const increaseProductQuantity = (productId: string) => {
     setProducts((prevProducts) => {
-      return prevProducts.map((prevPriduct) => {
-        if (prevPriduct.id != productId) {
-          return prevPriduct;
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
         }
-        return { ...prevPriduct, quantity: prevPriduct.quantity + 1 };
+        return { ...prevProduct, quantity: prevProduct.quantity + 1 };
       });
     });
   };
   const removeProduct = (productId: string) => {
     setProducts((prevProducts) =>
-      prevProducts.filter((prevProduct) => prevProduct.id != productId),
+      prevProducts.filter((prevProduct) => prevProduct.id !== productId),
     );
   };
   return (
@@ -93,12 +97,13 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       value={{
         isOpen,
         products,
-        total,
         toggleCart,
         addProduct,
         decreaseProductQuantity,
         increaseProductQuantity,
         removeProduct,
+        total,
+        totalQuantity,
       }}
     >
       {children}
