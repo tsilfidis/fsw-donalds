d3aa34a198edd84a61147583e5c6dc29d1928971
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-03-10 08:13:22 -0400

    chore: chege list orderlist page.

--- src/app/[slug]/orders/components/order-list.tsx
+++ src/app/[slug]/orders/components/order-list.tsx
@@ -60,7 +60,7 @@ const OrderList = ({ orders }: OrderListProps) => {
         <Card key={order.id}>
           <CardContent className="space-y-4 p-5">
             <div
-              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${[OrderStatus.PAYMENT_CONFIRMED, OrderStatus.FINISHED].includes(order.status) ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
+              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${([OrderStatus.PAYMENT_CONFIRMED, OrderStatus.FINISHED] as OrderStatus[]).includes(order.status) ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
             >
               {getStatusLabel(order.status)}
             </div>
